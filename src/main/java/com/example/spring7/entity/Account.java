65e4eb8efec5c9a9258c2e060afa8b0d44bb3a8e
Mgokayay <aymustafagokay@gmail.com>
2024-05-14 17:38:34 +0300

    account entity created

---
+++ src/main/java/com/example/spring7/entity/Account.java
@@ -0,0 +1,31 @@
+package com.example.spring7.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "account",schema = "fsweb")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_name")
+    private String accountName;
+
+    @Column(name = "money_amount")
+    private Double moneyAmount;
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+}
